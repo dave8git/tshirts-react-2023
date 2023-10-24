7eb1a35f4d042bbddb1ce04806f495552457c610
dave8git <dave8git@gmail.com>
2023-10-25 01:31:12 +0200

    Add useMemo to getPrice() truning it into price constant

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import styles from './Product.module.scss';
 import Button from '../Button/Button';
 import ProductImage from '../ProductImage/ProductImage';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import ProductForm from '../ProductForm/ProductForm';
 
 const Product = props => {
@@ -25,21 +25,21 @@ const Product = props => {
     console.log('działa');
     const summaryObject = {
         name: props.title,
-        price: getPrice(),
+        price: price,
         color: currentColor,
         size: currentSize,
     }
     console.log(summaryObject);
 }
 
-  const getPrice = () => {
+  const price = useMemo(() => {
     const selectedSize = props.sizes.find((size) => size.name === currentSize); // zwraca obiekt (znaleziony w array props.sizes) którego size.name jest takie samo jak w stanie lokalnym ustawionym w handleSize po kliknięciu na button rozmiaru koszulki
     if (selectedSize) { // jeżeli obiekt został znaleziony dodaj cenę dodatkową koszulki do ceny bazowej i zwróć, jak nie to zwróć orygnalną basePrice
       const totalPrice = props.basePrice + selectedSize.additionalPrice;
       return totalPrice;
     }
     return props.basePrice;
-  }
+  }, [currentSize])
 
 
 
@@ -51,7 +51,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductForm currentColor={currentColor} name={props.title}  setCurrentSize={setCurrentSize} setCurrentColor={setCurrentColor} colors={props.colors} sizes={props.sizes} size={currentSize} />
       </div>
